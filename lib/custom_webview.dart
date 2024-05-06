5710270eb3c4216029d639354848dc1d9ee1011a
hlongho <longho4697@gmail.com>
2024-05-06 11:42:20 +0700

    init

---
+++ lib/custom_webview.dart
@@ -0,0 +1,92 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+class CustomWebView extends StatefulWidget {
+  final String url;
+  const CustomWebView(this.url, {super.key});
+
+  @override
+  State<CustomWebView> createState() => _CustomWebViewState();
+}
+
+class _CustomWebViewState extends State<CustomWebView> {
+  late bool isPDFAndroid;
+  late final WebViewController _controller;
+
+  late File pFile;
+  bool isLoading = false;
+
+  Future<void> loadNetwork() async {
+    setState(() {
+      isLoading = true;
+    });
+    var url = widget.url;
+    final response = await http.get(Uri.parse(url));
+    final bytes = response.bodyBytes;
+    final filename = basename(url);
+    final dir = await getApplicationDocumentsDirectory();
+    var file = File('${dir.path}/$filename');
+    await file.writeAsBytes(bytes, flush: true);
+    if (mounted) {
+      setState(() {
+        pFile = file;
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    isPDFAndroid = widget.url.contains(".pdf", widget.url.length - 5) &&
+        Platform.isAndroid;
+    super.initState();
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+          allowsInlineMediaPlayback: false,
+          mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{
+            PlaybackMediaTypes.audio,
+            PlaybackMediaTypes.video
+          },
+          limitsNavigationsToAppBoundDomains: true);
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(params);
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..enableZoom(true)
+      ..setBackgroundColor(Colors.white);
+    // ..loadRequest(Uri.parse(widget.path!));
+    _controller = controller;
+    if (isPDFAndroid) {
+      loadNetwork();
+    } else {
+      _controller.loadRequest(Uri.parse(widget.url));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isPDFAndroid
+        ? isLoading
+            ? const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+              )
+            : PDFView(
+                filePath: pFile.path,
+                autoSpacing: false,
+                fitPolicy: FitPolicy.BOTH,
+              )
+        : WebViewWidget(controller: _controller);
+  }
+}
